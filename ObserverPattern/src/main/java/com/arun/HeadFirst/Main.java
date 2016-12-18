3b2ee930d95c4f95d4b806b614e337b956f737d0
arun789 <adwiti1975@gmail.com>
2016-12-18 02:12:49 -0700

    remove statistics display

--- ObserverPattern/src/main/java/com/arun/HeadFirst/Main.java
+++ ObserverPattern/src/main/java/com/arun/HeadFirst/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		CurrentConditionsDisplay ccd = new CurrentConditionsDisplay(weatherData);
 		StatisticsDisplay ssd = new StatisticsDisplay(weatherData);
 		weatherData.setMeasurement(12, 23, 56);
+		weatherData.removeObserver(ssd); 
 		weatherData.setMeasurement(45, 45, 23);
 	}
 
