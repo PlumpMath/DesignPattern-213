d0445a79acaa5a5aa1cc618c09234e5bfe176816
arun789 <adwiti1975@gmail.com>
2016-12-20 00:17:25 -0700

    definition of Observer pattern

--- ObserverPattern/src/main/java/com/arun/HeadFirst/Subject.java
+++ ObserverPattern/src/main/java/com/arun/HeadFirst/Subject.java
@@ -1,6 +1,11 @@
 package com.arun.HeadFirst;
 
 public interface Subject {
+	
+	/*
+	 * Observer pattern defines one to many relationship where change in state
+	 * of object is notified to all its dependents
+	 */ 
 	void registerObserver(Observer o);
 
 	void removeObserver(Observer o);
